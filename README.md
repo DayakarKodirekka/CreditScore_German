48bf891673b03622e5af9c02c0aa200fe7230cfb
Daya <dayakar.kodirekka@gmail.com>
2021-07-18 05:58:00 +0530

    Modified requirements file and added YAML workflow

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CreditScore_German
+
+
 Building model for credit score data 
 * Used SMOTE(Synthetic Minority Oversampling Technique) model to remove the imbalnce in the data
--- test_app.py
+++ test_app.py
@@ -15,10 +15,26 @@ def test_ping():
 def test_pred_loan():
     # defining a sample payload for the testcase
     payload = {
-        "sepal_length": 3,
-        "sepal_width": 5,
-        "petal_length": 3.2,
-        "petal_width": 4.4,
+        "Status_of_existing_checking_account": 'A11',
+        "Duration_in_month": 24,
+        "Credit_history": "A33",
+        "Purpose": "A40",
+        "Credit_amount": 4870,
+        "Savings_account_bonds": "A61",
+        "Present_employment_since": "A73",
+        "Installment_rate_in_percentage_of_disposable_income": 3,
+        "Personal_status_and_sex": "A93",
+        "Other_debtors_guarantors": "A101", # remove the blackslash from the column
+        "Present_residence_since": 4,
+        "Property": "A124",
+        "Age_in_years": 53,
+        "Other_installment_plans": "A143",
+        "Housing": "A153",
+        "Number_of_existing_credits_at_this_bank": 1,
+        "Job": "A173",
+        "Number_of_people_being_liable_to_provide_maintenance_for": 2,
+        "Telephone":"A191",
+        "foreign_worker": "A201"
     }
     with TestClient(app) as client:
         response = client.post("/predict_loan", json=payload)
